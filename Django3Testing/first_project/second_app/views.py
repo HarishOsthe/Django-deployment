1f656954f84a011426b2a3ac0d349adbf82170fc
VBS-analytics <analytics@valuestreambiz.com>
2020-02-12 17:33:46 +0530

    completed the django level 1 basics

--- Django3Testing/first_project/second_app/views.py
+++ Django3Testing/first_project/second_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index2(request):
-    return HttpResponse("<h1>osthe</h1>")
+    var1 = {'helpvar':'Help Page fromhelp.html'}
+
+    return render(request,'second_app/help.html',context=var1)
