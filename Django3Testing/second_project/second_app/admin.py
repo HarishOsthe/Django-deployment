0e46f2462cfa6856d34052392bebe8ee2355f47d
VBS-analytics <analytics@valuestreambiz.com>
2020-02-13 10:29:59 +0530

    Django Models has been created and tested using a django admin page

--- Django3Testing/second_project/second_app/admin.py
+++ Django3Testing/second_project/second_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from second_app.models import AccessRecords, Topic, WebPage
 # Register your models here.
+
+admin.site.register(AccessRecords)
+admin.site.register(Topic)
+admin.site.register(WebPage)
